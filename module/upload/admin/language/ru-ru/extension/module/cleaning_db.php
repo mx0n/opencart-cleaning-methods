a65a3092acea8e730ffdea40946f48c1c65b05a8
mx0n <crd.dev@gmail.com>
2018-09-20 22:27:11 +0300

    Добавляет файлы модуля

---
+++ module/upload/admin/language/ru-ru/extension/module/cleaning_db.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Russian 
+ * 
+ */
+
+ // Heading
+$_['heading_title']    = 'Cleaning-Db';
+
+// Text
+$_['text_module']      = 'Модули';
+$_['text_total']       = 'Всего заказов';
+$_['text_payment']     = 'Оплата';
+$_['text_shipping']    = 'Доставка';
+$_['text_feed']        = 'Канал продвижения';
+$_['text_success']     = 'Настройки модуля Cleaning-Db обновлены!';
+$_['placeholder']      = 'Поздравляем! Вы успешно ';
+
+// Error
+$_['error_permission'] = 'Ошибка: У Вас нет прав на изменение настроек модуля Cleaning-Db!';
+
+?>
